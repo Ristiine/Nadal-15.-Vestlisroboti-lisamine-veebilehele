66bd3f5a116822e606b26f1b30ea302abdc841bd
Ristiine R <ristiine.ruven@tkt.ee>
2023-05-12 13:00:41 +0300

    viimane projekt

---
+++ public/scripts/app.js
@@ -0,0 +1,43 @@
+const movieTitle = document.querySelector('.movie-info-title');
+const releaseDate = document.querySelector('.release-date');
+const movieGenres = document.querySelector('.genres');
+const movieDuration = document.querySelector('.runtime');
+//const moviePoster = document.querySelector('.movie-poster-container img');
+const movieQuote = document.querySelector('.movie-info-quote');
+const movieOverview = document.querySelector('.movie-info-overview');
+//const footerYear = document.querySelector('year'); -- See ei tööta miskipärast
+
+
+window.onload = () => {
+
+    let url = 'https://api.themoviedb.org/3/movie/293660?api_key=03f71e864ceef92eda9743005dd1ab52';
+
+    fetch(url)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        movieTitle.textContent = data.title;
+       
+        let date = new Date(data.release_date);
+        releaseDate.textContent = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()} ${data.production_countries[0].iso_3166_1}`;
+        movieDuration.textContent = `${data.runtime} minutes.`;
+        movieQuote.textContent = data.tagline;
+        movieOverview.textContent = data.overview;
+
+        /*let posterUrl = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${data.poster_path}`;
+        moviePoster.src = posterUrl;
+        moviePoster.alt = `${data.title} Poster`;*/
+        
+        let genres = '';
+        data.genres.forEach(genre => {
+            genres = genres + `${genre.name}, `;
+        });
+        
+        let genresUpdated = genres.slice(0, -2) + '.';
+        movieGenres.textContent = genresUpdated;
+
+        //let currentYear = new Date().getFullYear(); -- See ei tööta miskipärast
+       // footerYear.textContent = currentYear;
+    });
+}
